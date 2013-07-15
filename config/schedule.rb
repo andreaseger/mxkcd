fc8be89f000ccd8cfef7cb14b4ab51675aed8cb5
Andreas Eger <dev@eger-andreas.de>
2013-07-16 01:19:48 +0200

    remove whenever schedule

--- config/schedule.rb
+++
@@ -1,7 +0,0 @@
-job_type :ruby, 'cd :path && :environment_variable=:environment bundle exec ruby :task'
-
-# monday, wednesday, friday
-# a few times between 3 and 7 o'clock
-every '0,1,5,30,55 3-7 * * 1,3,5' do
-  ruby 'mxkcd.rb'
-end
