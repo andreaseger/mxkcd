759d6a125de8d9d2a76ba55e1fdd8a3f4c60d531
Andreas Eger <dev@eger-andreas.de>
2013-07-17 09:27:58 +0200

    fix job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # Changing directories to your project and load rvm gemset
-cd ~/code/mxkcd
+cd /srv/cron/mxkcd
 source ~/.rvm/environments/ruby-2.0.0-p247-turbo@mxkcd
 
 # call script via bundler
